fc79c246f05d4f306644aab8d6839840417a5651
siraphat <siraphat975@gmail.com>
2023-01-16 19:19:20 +0700

    change function to class in Token manager

--- src/token/tokenManager.ts
+++ src/token/tokenManager.ts
@@ -1,9 +1,37 @@
 import jwt from 'jsonwebtoken';
+import { payload } from '../types/types';
 
-export const generateAcessToken = (payload : Object) => {
-    return jwt.sign(payload, process.env.SECRET_KEY_ACESS, { expiresIn: '15s' });
+class TokenManager {
+    
+    public payload : payload
+
+    protected accessToken : string;
+    protected refreshToken : string 
+
+    protected invalidRefreshToken : Array<string>  = []
+    
+    constructor(payload : payload){
+        this.payload = payload;
+    }
+
+    generateAcessToken(){
+        this.accessToken = jwt.sign(this.payload, process.env.SECRET_KEY_ACESS);
+        return this.accessToken;
+    }
+
+    generateRefreshToken(){
+        this.refreshToken = jwt.sign(this.payload, process.env.SECRET_KEY_REFRESH);
+        return this.refreshToken
+    }
+
+    validRefreshToken(refreshToken : string){
+        return this.invalidRefreshToken.includes(refreshToken)
+    }
+
+    invalidToken(refreshToken){
+        this.invalidRefreshToken.push(refreshToken)
+    }
+
 }
 
-export const generateRefreshToken = (payload : Object) => {
-    return jwt.sign(payload, process.env.SECRET_KEY_REFRESH);
-}
+export default TokenManager;
