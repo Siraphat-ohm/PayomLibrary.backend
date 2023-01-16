af32f048cf920345b8afe794f15e72b41bb20a72
siraphat <siraphat975@gmail.com>
2023-01-17 00:59:21 +0700

    add request prop

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,4 +1,14 @@
+import { Request } from "express"
+
 export type payload  = { 
     user: string,
     role: number
+}
+
+declare module "express" {
+    export interface Request {
+        user? : string
+        role? : number
+        token? : string
+    }
 }
