eb0d5e179658355ed33fb856057984afeb12e176
siraphat <siraphat975@gmail.com>
2023-01-16 19:20:15 +0700

    use jwt in login

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,32 +1,32 @@
 import { Request, Response } from "express";
 import { User } from "../entity/User";
-import jwt from "jsonwebtoken";
-import roles from "../config/roles.json"
-import { generateAcessToken } from "../token/tokenManager";
-
 const bcrypt = require('bcrypt');
+import TokenManager from "../token/tokenManager";
+import { payload } from "../types/types";
 
 const handleLogin = async (req:Request, res:Response, next:Function) => {
     let { user, pwd } = req.body;
+
     try {
         let foundUser = await User.find( { where: { userName:req.body.user }})
-
+        
         const userInput : string = foundUser[0].userName
         const pwdInput : string = foundUser[0].passWord
-        const role : string = roles[foundUser[0].role]
+        const role : number = foundUser[0].role
         
-        const payload : Object = { user:userInput, role:role }
-
-        const acessToken : string = generateAcessToken(payload);
-        const refreshToken : string = generateAcessToken(payload);
+        const payload : payload = { user:userInput, role:role }
+        const TKM = new TokenManager(payload)
 
         if (userInput == user && await bcrypt.compareSync(pwd, pwdInput)) {
-            res.json( { acessToken : acessToken, refreshToken : refreshToken, auth : true } )
+            const accessToken = TKM.generateAcessToken()
+            const refreshToken = TKM.generateRefreshToken()
+
+            res.json({ accessToken: accessToken, refreshToken: refreshToken, auth: true})
         }
+
     } catch (error) {
         next(error); 
     }
-    
 }
 
 export default { handleLogin }
