61f569514d14ac1ce3149513333e2a4edf01e449
siraphat <siraphat975@gmail.com>
2023-01-22 11:42:29 +0700

    refresh route

---
+++ src/routes/refresh.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import refreshTokenController from "../controller/refreshTokenController";
+
+const router = express.Router();
+
+router.get('/', refreshTokenController.handleRefreshToken);
+
+export = router;
