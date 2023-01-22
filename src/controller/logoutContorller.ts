dea55546e5689b444979c93981d84229e1fe77a7
siraphat <siraphat975@gmail.com>
2023-01-22 11:39:07 +0700

    logout with cookie

--- src/controller/logoutContorller.ts
+++ src/controller/logoutContorller.ts
@@ -1,9 +1,24 @@
 import { Request, Response } from "express";
+import { User } from "../entity/User";
+import TokenManager from "../token/tokenManager";
 
 
-const handleLogout = (req:Request, res:Response, next:Function) => {
-    req.session.destroy((err) => {}) //destroy session .
-    res.json({"session":req.session})
+const handleLogout = async(req:Request, res:Response, next:Function) => {
+
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.status(204).json( { "message":"don't have cookies" }); // No content
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await User.find( { where: { refreshToken : refreshToken } })
+    if(foundUser.length == 0) {
+        res.clearCookie('jwt', { httpOnly : true, sameSite:"none", secure:true })
+        return res.sendStatus(204).json({ "message":"not found user" });
+    }
+
+    await User.update({ refreshToken : refreshToken }, { refreshToken : ""})
+    res.clearCookie('jwt', { httpOnly : true, sameSite:"none", secure:true })
+    return res.status(204).json( { "message" : "clear cookies"} );
+
 }
 
 export default { handleLogout }
