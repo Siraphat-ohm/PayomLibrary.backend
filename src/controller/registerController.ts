d98b8a4711f5f1fad1f3d567f6a7af1ccf8be40f
siraphat <siraphat975@gmail.com>
2023-01-22 11:40:20 +0700

    check user duplicate

--- src/controller/registerController.ts
+++ src/controller/registerController.ts
@@ -4,13 +4,18 @@ const bcrypt = require('bcrypt')
 
 const handleRegister = async(req:Request, res:Response, next:Function) => {
     let { user, pwd } = req.body;
+    if(!user || !pwd) return res.status(400).json({"message": "Username and password are required"})
+
+    const duplicate = User.find( { where : { userName : user}});
+    if(duplicate) return res.sendStatus(409);
+
     try {
         let pwdHash:string = bcrypt.hashSync(pwd, 10);
 
         await User.insert({ userName:user, passWord:pwdHash });
         res.sendStatus(200);
     } catch (error) {
-       next(error); 
+        next(error); 
     }
 }
 
