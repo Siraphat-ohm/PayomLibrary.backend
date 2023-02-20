9c24eb60a3235247f8b1341904e76c4c0ab286fe
siraphat <siraphat975@gmail.com>
2023-02-20 14:19:55 +0700

    t

--- src/controller/bookController.ts
+++ src/controller/bookController.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
-import * as fs from 'fs'
 import { Book } from "../entity/Book";
 
 const paginationBooks = async (req:Request, res:Response, next:Function) => {
 
-    let page = 3;
+    let page = req.params.page;
     const size = 10;
 
     const calSkip = (page:number, size:number) => {
@@ -45,4 +44,47 @@ const paginationBooks = async (req:Request, res:Response, next:Function) => {
 
 }
 
-export default { paginationBooks }
+const getPage = async(req: Request, res: Response, next:Function) => {
+    try {
+        const data = await Book.count()
+        const page = Math.ceil(data/10);
+        res.json(
+            page
+        )
+    } catch (error) {
+        res.send(404);
+    }
+
+}
+
+const getBookById = async(req: Request, res: Response, next:Function) => {
+    const id = req.params.id 
+    console.log(id)
+
+    try {
+        const data = await Book.findOne( { where: { id: id }, relations: { 
+                                                    authors: true, 
+                                                    categories: true, 
+                                                    language: true 
+                                                } } )
+        const book = {
+            id: data.id,
+            title: data.title,
+            description: data.description,
+            ISBN: data.ISBN,
+            pubYear: data.pubYear,
+            page: data.page,
+            amount: data.copies,
+            thumbnail: data.thumbnail,
+            authors: data.authors.map(item => item.name),
+            category: data.categories.map(item => item.name),
+            language: data.language.language
+        }
+        res.json( book )
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(404);
+    }
+}
+
+export default { paginationBooks, getPage, getBookById }
--- src/routes/client/api/book.ts
+++ src/routes/client/api/book.ts
@@ -5,7 +5,8 @@ import roles from "../../../config/roles.json"
 
 const router = express.Router();
 
-router.route('/:page').get(verifyRoles(roles.student), bookController.paginationBooks );
-
+router.route('/page').get( verifyRoles(roles.student), bookController.getPage );
+router.route('/by/:page').get( verifyRoles(roles.student), bookController.paginationBooks );
+router.route('/:id').get( verifyRoles(roles.student), bookController.getBookById )
 
 export = router;
